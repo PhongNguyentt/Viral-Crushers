1b45f487e0f492e53d16f4561a3dc56ea1949f02
Phong Nguyen <nguyenphong@seattleu.edu>
2021-01-29 16:35:06 -0800

    Add project files.

---
+++ Game/Game/AudioEngine/AudioResources.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.AudioEngine
+{
+    /// <summary>
+    /// Collection of game sounds that are loaded at compilation time.
+    /// TODO trigger menu click sounds via an inherited menu class
+    /// with preset button handlers (get rid of globals)
+    /// </summary>
+    public class AudioResources
+    {
+        /// <summary>
+        /// Default menu click sound
+        /// </summary>
+        public static AudioClip MENU_CLICK_SOUND = new AudioClip(
+            filepath: "Game.AudioFiles.Music.Menu.TestMX1_Stinger_Bells.wav",
+            startingVolume: .5
+        );
+
+        public static AudioClip MX_MENU_SONG1_FULL_LOOP = new AudioClip(
+           filepath: "Game.AudioFiles.Music.Menu.VC-OST_Song1_Full_Loop.mp3",
+           startingVolume: .8,
+           loop: true
+       );
+    }
+}
---
+++ Game/Game/Views/Home/HomePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public HomePage()
+		{
+			InitializeComponent();
+			AudioEngine.AudioResources.MX_MENU_SONG1_FULL_LOOP.Play(); // Start menu music loop
+		}
+
+		/// <summary>
+		/// Example of a Button Click (this one is Sync, if calling Async then needs to be Async)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		public async void GameButton_Clicked(object sender, EventArgs e)
+        {
+			//AudioEngine.AudioResources.MENU_CLICK_SOUND.Play(); // Play test click sound
+			await Navigation.PushAsync(new GamePage());
+		}
+    }
+}
